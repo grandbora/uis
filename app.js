f43d56ec5e37b9e8fb90ed3e04f6aa35a18e932b
stadolf <stadolf@gmail.com>
2012-09-22 14:45:41 +0200

    added /tag route

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/tag', routes.tagsIndex);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.tagsIndex = function(req, res) {
+  res.send("tags");
+};
