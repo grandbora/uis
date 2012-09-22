7ade8d34fcf580237013a8ba35b504aad448855d
stadolf <stadolf@gmail.com>
2012-09-22 15:53:14 +0200

    switched to mongoskin

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,7 @@
  * GET home page.
  */
 
-var MNG = require('mongodb');
-
-var MONGO = {
-    host: 'ds037907.mongolab.com',
-    port:37907,
-    opts: {},
-    dbname: 'heroku_app7810183',
-    user: 'heroku_app7810183',
-    pass: 'qmoact0ipl86o21hl39sdabub8'
-
-};
-
-
+var mongodb = require('mongoskin');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
@@ -23,26 +11,12 @@ exports.index = function(req, res){
 
 exports.tagsIndex = function(req, res) {
 
+  var db= mongodb.db(process.env.MONGOLAB_URI);
+  var col = db.collection("uis");
 
-  var db = new MNG.Db(MONGO.dbname,
-      new MNG.Server(MONGO.host, MONGO.port,  MONGO.opts), {native_parser: false});
+  col.findOne({type:'t'}, function(err, doc) {
+        res.send(doc);
+  });
 
-    db.open(function(err, db) {
-        //assert.equal(null, err);
-        console.log("connected");
-        console.dir(err);
-
-        db.authenticate(MONGO.user, MONGO.pass, function(err, result) {
-            console.dir(err);
-
-            var collection = db.collection("uis");
-            collection.findOne({type:'t'}, function(err, doc) {
-                res.send(doc);
-
-            });
-
-        });
-
-    });
 
 };
